52d3d80e2cfd657cf4ac1d6aab18e4998644fd72
aiv55 <isaac@ziqquforge.com>
2015-10-07 21:52:55 +0000

    Test editing todo lists

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -10,7 +10,6 @@ describe "Creating todo lists" do
         fill_in "Title", with: options[:title]
         fill_in "Description", with: options[:description]
         click_button "Create Todo list"
-        
     end
     
     it "redirects to the todo list index page on success" do
