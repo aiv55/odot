e9ccdcb7d9a75c7ca9071c1d39042e328d4793f2
aiv55 <isaac@ziqquforge.com>
2016-02-01 14:12:12 +0000

    added user model validations and validation tests

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -5,22 +5,22 @@ describe "Viewing todo items" do
     
     it "displays the title of the todo list" do
         visit_todo_list(todo_list)
-        within("h1") do
+        within("#todo_list_title") do
             expect(page).to have_content(todo_list.title)
         end
     end
 
     it "displays no items when a todo list is empty" do
         visit_todo_list(todo_list)
-        expect(page.all("ul.todo_items li").size).to eq(0)
+        expect(page.all(".todo_items tbody tr").size).to eq(0)
     end
     
     it "displays item content when a todo list has items" do
         todo_list.todo_items.create(content: "Milk")
         todo_list.todo_items.create(content: "Eggs")
         visit_todo_list(todo_list)
-        expect(page.all("ul.todo_items li").size).to eq(2)
-        within "ul.todo_items" do
+        expect(page.all(".todo_items tbody tr").size).to eq(2)
+        within ".todo_items" do
             expect(page).to have_content("Milk")
             expect(page).to have_content("Eggs")
         end
