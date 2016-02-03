2ee6c13ceec5c1f578c5d8d4e4ed385c650ec668
aiv55 <isaac@ziqquforge.com>
2016-02-03 01:42:05 +0000

    Finished scoping users to todo_lists with associated tests

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
-    let(:user) { create(:user) }
-    let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.")}
+    let(:user) { todo_list.user }
+    let!(:todo_list) { create(:todo_list) }
     
     def update_todo_list(options={})
         options[:title] ||= "New title"
